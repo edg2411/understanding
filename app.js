0b43936535dd9b61405f7749369581b750858bdd
Ezequiel Di Giulio <eze.digiulio@gmail.com>
2020-03-21 02:07:24 -0300

    second commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// console.log('holamundo!');
+
+// // var person = new Object();
+// var person = {};
+
+// person['firstname'] = 'ezequiel';
+// person['lastname'] = 'di giulio';
+
+// propertyFirstName = 'firstname';
+// propertyLastName = 'lastname';
+
+// console.log(person);
+// console.log(person[propertyFirstName]);
+
+// console.log(person.firstname);
+
+
+var english = {
+    greetings: {
+        basic: 'hello!'
+    }
+};
+
+console.log(english);
