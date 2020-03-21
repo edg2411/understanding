d00203bc9adc38439998d08333adeab6633b5332
Ezequiel Di Giulio <eze.digiulio@gmail.com>
2020-03-21 15:45:57 -0300

    v4.38

--- app.js
+++ app.js
@@ -1,54 +1,20 @@
-console.log(this);
+var arr1 = new Array(); // una forma de declararlo
 
-function a() {
-    console.log(this);
-    this.newvar = 'hello';
-}
+var arr = [ // otra forma de declararlo
+    1,
+    false,
+    { name: 'eze' },
+    function(name) {
+        var greeting = 'hello ';
+        console.log(greeting + name);
+    },
+    "hi"
+];
 
-var b = function() {
-    console.log(this);
-}
+// distintos elementos del array no necesariamente tienen que ser del mismo tipo
 
-a();
+console.log(arr);
 
-console.log(newvar);
+arr[3](arr[2].name);
 
-b();
-
-// en todos los casos anteriores this apuntaba al objeto global window
-// al crear un obj literal el this apunta a ese obj y no al glob
-var c = {
-    name: 'obj c',
-    log: function() {
-        this.name = 'updt obj c';
-        console.log(this);
-
-        var setname = function(newname) {
-            //se supone que this dentro de esta funcion sigue apuntando al objeto c pero no..
-            this.name = newname;
-            //es mas.. este this apunta al objeto global, aunque este dentro de un objeto dentro de una funcion etc..
-        };
-        setname('new updt obj c');
-        console.log(this);
-    }
-};
-// considerado bug
-c.log();
-
-var d = {
-    name: 'obj d',
-    log: function() {
-        var self = this; // self o that le llaman apunta all objeto d
-        this.name = 'updt obj d';
-        console.log(self);
-
-        var setname = function(newname) {
-            // va a encontrar self (el engine) afuera de function (scope chain) y que apunta al obj d
-            self.name = newname;
-        };
-        setname('new updt obj d');
-        console.log(self);
-    }
-};
-
-d.log();
+// 3 es el indice de la funcion, 2 el indice del objeto y .name el valor que contiene (value-pair)
