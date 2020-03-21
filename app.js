382d33ad6d6760dc452f4b4acbc2198bc751d043
Ezequiel Di Giulio <eze.digiulio@gmail.com>
2020-03-21 15:16:58 -0300

    s4.37

--- app.js
+++ app.js
@@ -1,39 +1,54 @@
-//  by value
+console.log(this);
 
-var a = 3;
-var b;
-
-b = a;
-a = 2;
-
-console.log(a);
-console.log(b);
-
-// by reference ( all obj including func)
-
-var c = { greeting: 'hi' };
-var d;
-
-d = c;
-c.greeting = 'hello'; // mutate
-
-console.log(c);
-console.log(d);
-
-// by ref paramenters
-
-function changeGreeting(obj) {
-    obj.greeting = 'Hola';
+function a() {
+    console.log(this);
+    this.newvar = 'hello';
 }
 
-changeGreeting(d);
+var b = function() {
+    console.log(this);
+}
 
-console.log(c);
-console.log(d);
+a();
 
-// equals (=) sets uop new mem space (new address)
+console.log(newvar);
 
-c = { greeting: 'howdy' }; // crea un espacio para howdy y luego apunta c a esa direccion
+b();
 
-console.log(c);
-console.log(d);
+// en todos los casos anteriores this apuntaba al objeto global window
+// al crear un obj literal el this apunta a ese obj y no al glob
+var c = {
+    name: 'obj c',
+    log: function() {
+        this.name = 'updt obj c';
+        console.log(this);
+
+        var setname = function(newname) {
+            //se supone que this dentro de esta funcion sigue apuntando al objeto c pero no..
+            this.name = newname;
+            //es mas.. este this apunta al objeto global, aunque este dentro de un objeto dentro de una funcion etc..
+        };
+        setname('new updt obj c');
+        console.log(this);
+    }
+};
+// considerado bug
+c.log();
+
+var d = {
+    name: 'obj d',
+    log: function() {
+        var self = this; // self o that le llaman apunta all objeto d
+        this.name = 'updt obj d';
+        console.log(self);
+
+        var setname = function(newname) {
+            // va a encontrar self (el engine) afuera de function (scope chain) y que apunta al obj d
+            self.name = newname;
+        };
+        setname('new updt obj d');
+        console.log(self);
+    }
+};
+
+d.log();
