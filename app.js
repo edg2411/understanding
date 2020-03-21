daca0815b017654b66808f0356e4afa6a0965776
Ezequiel Di Giulio <eze.digiulio@gmail.com>
2020-03-21 17:09:11 -0300

    v4.44

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
-function getPerson() {
-    // return; {
-    return {
-        firstname: 'eze'
-    }
+// using f statement
+
+function greet(name) {
+    console.log('hola ' + name);
 }
 
-console.log(getPerson());
+greet('eze');
 
-// directamente el vsc no me deja hacer lo que queria demostrar pero
-// el engine de js pone automaticamente los ; donde cree que van si no los pones vos
+// using f expression
+
+var greetF = function(name) {
+    console.log('hola ' + name);
+};
+
+greetF('eze');
+
+// using IIFE(immediately invoked function expression)
+
+var greeting = function(name) {
+    console.log('hola ' + name);
+}();
+
+var greeting2 = function(name) {
+    return 'hola ' + name;
+}('eze');
+// console.log(greeting2());    // esto tira error pq greeting2 ahora tiene un string!
+console.log(greeting2);
+
+3;
+'hola';
+
+{
+    name: 'eze'
+};
+
+// no funciona pq lo reconoce como un f statement y no puede no tener nombre! por eso token(
+// function(name) {
+//     return 'hello' + name;
+// };
+
+// el engine reconoce que lo que esta entre () debe ser una expresion
+
+var firstname = 'eze';
+
+(function(name) {
+    // return 'hello' + name;
+    var greeting = 'inside IIFE: hello ';
+    console.log(greeting + name);
+}(firstname)); // de hecho si no la invoco el vsc se queja
+// })(firstname); // tambien es valido con los () de invocacion fuera de los () de wrapping
