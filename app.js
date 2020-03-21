f7659d62305a43ebfc379faf4a4c89f379a83932
Ezequiel Di Giulio <eze.digiulio@gmail.com>
2020-03-21 16:30:06 -0300

    v4.42

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-// en js el overload o sobrecarga de funciones no es igual que en otros lenguajes
-// pero al tener first class functions se puede manejar 
-// esta e suna forma de salvar problemas, no es la mejor ni la unica!
-
-function greet(firstname, lastname, language) {
-
-    language = language || 'en'; // esto sirve tambien!
-
-    if (language === 'en') {
-        console.log('hello ' + firstname + ' ' + lastname);
-    }
-    if (language === 'es') {
-        console.log('hola ' + firstname + ' ' + lastname);
+function getPerson() {
+    // return; {
+    return {
+        firstname: 'eze'
     }
 }
 
-function greetEnglish(firstname, lastname) {
-    greet(firstname, lastname, 'en');
-}
+console.log(getPerson());
 
-function greetSpanish(firstname, lastname) {
-    greet(firstname, lastname, 'es');
-}
-
-greetEnglish('eze', 'dg');
-greetSpanish('eze', 'dg');
+// directamente el vsc no me deja hacer lo que queria demostrar pero
+// el engine de js pone automaticamente los ; donde cree que van si no los pones vos
