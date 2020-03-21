6e810b10748c6b9134496576652fb246f225be6a
Ezequiel Di Giulio <eze.digiulio@gmail.com>
2020-03-21 03:14:36 -0300

    s4.35

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
+// first class functions    en js las funciones son objetos y punto
+
+console.log('normal');
+
+
+// greet(); // esto funciona 
+
+//statement
 function greet() {
     console.log('hi');
 }
 
-greet.language = 'english';
+greet(); // esto funciona 
 
 console.log(greet());
 
-greet();
+console.log('anonima');
+
+
+// anonymousGreet(); // esto no funciona!
+
+// la variable esta hoisteada pero no tiene nada cargado (undefined) y por eso tira error al poner ()
+
+//expression
+var anonymousGreet = function() {
+    console.log('hi');
+}
+
+anonymousGreet(); // esto funciona 
+
+console.log(anonymousGreet);
+console.log(anonymousGreet());
+
+// nota: pasa lo mismo del undefined
+
+
+function log(a) {
+    a();
+}
+
+//function expression
+
+log(function() {
+    console.log('hi');
+});
